add916c72ab44d36618ce058f29b13cc3298d9a7
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-25 16:54:15 +0500

    add prettier

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,35 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-import logoSvg from '../assets/img/pizza-logo.svg';
-import { Search } from './';
-import { selectCart } from '../redux/cart/selectors';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link, useLocation } from 'react-router-dom'
+import logoSvg from '../assets/img/pizza-logo.svg'
+import { selectCart } from '../redux/cart/selectors'
+import { Search } from './'
 
 export const Header: React.FC = () => {
-  const { items, totalPrice } = useSelector(selectCart);
-  const location = useLocation();
-  const isMounted = React.useRef(false);
+  const { items, totalPrice } = useSelector(selectCart)
+  const location = useLocation()
+  const isMounted = React.useRef(false)
 
-  const totalCount = items.reduce((sum: number, item: any) => sum + item.count, 0);
+  const totalCount = items.reduce((sum: number, item: any) => sum + item.count, 0)
 
   React.useEffect(() => {
     if (isMounted.current) {
-      const json = JSON.stringify(items);
-      localStorage.setItem('cart', json);
+      const json = JSON.stringify(items)
+      localStorage.setItem('cart', json)
     }
-    isMounted.current = true;
-  }, [items]);
+    isMounted.current = true
+  }, [items])
 
   return (
     <div className="header">
       <div className="container">
         <Link to="/">
           <div className="header__logo">
-            <img width="38" src={logoSvg} alt="Pizza logo" />
+            <img
+              width="38"
+              src={logoSvg}
+              alt="Pizza logo"
+            />
             <div>
               <h1>Pizza</h1>
               <p>самая вкусная пицца во вселенной</p>
@@ -36,7 +39,10 @@ export const Header: React.FC = () => {
         {location.pathname !== '/cart' && <Search />}
         <div className="header__cart">
           {location.pathname !== '/cart' && (
-            <Link to="/cart" className="button button--cart">
+            <Link
+              to="/cart"
+              className="button button--cart"
+            >
               <span>{totalPrice} ₽</span>
               <div className="button__delimiter"></div>
               <svg
@@ -44,7 +50,8 @@ export const Header: React.FC = () => {
                 height="18"
                 viewBox="0 0 18 18"
                 fill="none"
-                xmlns="http://www.w3.org/2000/svg">
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path
                   d="M6.33333 16.3333C7.06971 16.3333 7.66667 15.7364 7.66667 15C7.66667 14.2636 7.06971 13.6667 6.33333 13.6667C5.59695 13.6667 5 14.2636 5 15C5 15.7364 5.59695 16.3333 6.33333 16.3333Z"
                   stroke="white"
@@ -73,5 +80,5 @@ export const Header: React.FC = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
